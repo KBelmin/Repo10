5fc721820d41fbcb8847c8e12c9b9847ee421529
KBelmin <ariardorn@icloud.com>
2022-04-06 23:19:25 +0200

    Changes

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -8,7 +8,9 @@ function myFunction(myObj) {
     var vegetableValue = myObj["vegetable"];
     var fruitValue = myObj["fruit"];
     var drinkValue = myObj["drink"];
-    return { vegetableValue, fruitValue, drinkValue }
+    return { vegetableValue, fruitValue, drinkValue };
 }
-console.log (myFunction(myFood));
+
+console.log(myFunction(myFood));
+
 module.exports = myFunction(myFood);
